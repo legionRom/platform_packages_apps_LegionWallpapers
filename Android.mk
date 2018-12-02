a2a8b9a05deb99ff21d5f9ea5a2311c1f0b0d0c2
RakeshBatra <raakesh.batra@rediffmail.com>
2018-12-02 15:50:58 +0000

    Changes In Line with Pie FWB Support

--- Android.mk
+++ Android.mk
@@ -6,7 +6,6 @@
 # The text of the license can be found in the LICENSE file
 # or at https://www.gnu.org/licenses/gpl-2.0.txt
 #
-
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -14,20 +13,14 @@ LOCAL_SRC_FILES += $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := current
+LOCAL_USE_AAPT2 := true
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat \
     android-support-design
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/design/res
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v4 \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.design
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_PACKAGE_NAME := ColtPapers
 
