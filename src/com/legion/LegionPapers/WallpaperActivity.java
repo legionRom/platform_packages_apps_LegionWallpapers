182943f67ff9239e1c53e737d0344308340a6fdc
rajkale99 <kaleraj.rk@gmail.com>
2019-04-15 08:30:04 +0000

    Adapt For LegionPaper

--- src/com/legion/LegionPapers/WallpaperActivity.java
+++ src/com/legion/LegionPapers/WallpaperActivity.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package com.colt.ColtPapers;
+package com.legion.LegionPapers;
 
 import android.app.Activity;
 import android.app.AlertDialog;
